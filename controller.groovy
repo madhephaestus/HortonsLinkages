d211665030eb113734153c1e39fc084415fb3c26
Kevin Harrington <mad.hephaestus@gmail.com>
2020-10-23 23:47:22 -0400

    working parametrics

--- controller.groovy
+++ controller.groovy
@@ -1,6 +1,13 @@
+import com.neuronrobotics.bowlerstudio.creature.MobileBaseCadManager
 import com.neuronrobotics.bowlerstudio.creature.MobileBaseLoader
+import com.neuronrobotics.sdk.addons.kinematics.AbstractLink
 import com.neuronrobotics.sdk.addons.kinematics.DHParameterKinematics
+import com.neuronrobotics.sdk.addons.kinematics.ILinkListener
 import com.neuronrobotics.sdk.addons.kinematics.MobileBase
+import com.neuronrobotics.sdk.addons.kinematics.math.RotationNR
+import com.neuronrobotics.sdk.addons.kinematics.math.TransformNR
+import com.neuronrobotics.sdk.common.DeviceManager
+import com.neuronrobotics.sdk.pid.PIDLimitEvent
 
 import javafx.beans.value.ChangeListener
 import javafx.beans.value.ObservableValue
@@ -82,28 +89,26 @@ public class HortonsController {
 	void liveMove(MouseEvent event) {
 		double lower = Double.parseDouble(startAngle.getText())
 		double upper = Double.parseDouble(endAngle.getText())
+
 		double range =lower+( (upper-lower)*positionSlider.getValue()/100.0);
 		println "Live move "+range
-		
+
 		setLinkAngle(range)
 	}
-	
+
 	void setLinkAngle(double angle) {
 		if(angle.isNaN())
 			return;
 		new Thread() {
-			void run() {
-				def limb =system.getAllDHChains().get(0)
-				DHParameterKinematics follower = system.getAllDHChains().get(1)
-				def crank =limb.getAbstractLink(0)
-				crank.setTargetEngineeringUnits(angle)
-				crank.flush(0);
-				Thread.sleep(20)
-				def tipOfCrank= limb.getCurrentTaskSpaceTransform()
-				follower.setDesiredTaskSpaceTransform(tipOfCrank, 0)
-			}
-		}.start();
-		
+					void run() {
+						def limb =system.getAllDHChains().get(0)
+						DHParameterKinematics follower = system.getAllDHChains().get(1)
+						def crank =limb.getAbstractLink(0)
+						crank.setTargetEngineeringUnits(angle)
+						crank.flush(0);
+						
+					}
+				}.start();
 	}
 
 	@FXML
@@ -113,10 +118,33 @@ public class HortonsController {
 
 	@FXML
 	void update(ActionEvent event) {
-		println "Update Hortons"
-		
-		
-		
+		new Thread() {
+					void run() {
+						println "Update Hortons"
+						double lower = Double.parseDouble(startAngle.getText())
+						double upper = Double.parseDouble(endAngle.getText())
+						double increment=  Double.parseDouble(deltaAngle.getText())
+						double speed = Double.parseDouble(velocityOfCrank.getText())
+						double range = (upper-lower)
+						double secondsPerRange = range/speed
+						double numIncrements = range/increment
+						println "Seconds per range "+secondsPerRange
+
+						system.getAllDHChains().get(0).setDH_R(0, Double.parseDouble(lenLin2.getText()))
+						system.getAllDHChains().get(1).setDH_R(2, Double.parseDouble(lenLin3.getText()))
+						system.getAllDHChains().get(1).setDH_R(1, Double.parseDouble(lenLin4.getText()))
+						TransformNR tf = system.getAllDHChains().get(1).getRobotToFiducialTransform();
+						tf.setY(-Double.parseDouble(lenLin1.getText()))
+						system.getAllDHChains().get(1).setRobotToFiducialTransform(tf)
+						for(double i=lower;i<=upper;i+=increment) {
+							setLinkAngle(i)
+							Thread.sleep((long)(secondsPerRange*1000.0/numIncrements))
+						}
+						setLinkAngle(0)
+						if(event !=null)
+							MobileBaseCadManager.get(system).generateCad();
+					}
+				}.start();
 	}
 
 	@FXML // This method is called by the FXMLLoader when initialization is complete
@@ -160,12 +188,36 @@ public class HortonsController {
 		endAngle.focusedProperty().addListener(cl); // Value injected by FXMLLoader
 		deltaAngle.focusedProperty().addListener(cl); // Value injected by FXMLLoader
 		velocityOfCrank.focusedProperty().addListener(cl); // Value injected by FXMLLoader
-		
+
 		system = DeviceManager.getSpecificDevice( "FourBarGroup",{
 			MobileBase m = MobileBaseLoader.fromGit("https://github.com/WPIRoboticsEngineering/HortonsLinkages.git", "fourbar.xml")
 			return m
 		})
 		
+		DHParameterKinematics limb =system.getAllDHChains().get(0)
+		DHParameterKinematics follower = system.getAllDHChains().get(1)
+		AbstractLink crank =limb.getAbstractLink(0)
+		crank.addLinkListener(new ILinkListener() {
+			/**
+			 * On link position update.
+			 *
+			 * @param source the source
+			 * @param engineeringUnitsValue the engineering units value
+			 */
+			public void onLinkPositionUpdate(AbstractLink source,double engineeringUnitsValue) {
+				def tipOfCrank= limb.getCurrentTaskSpaceTransform()
+				follower.setDesiredTaskSpaceTransform(tipOfCrank, 0)
+			}
+			public void onLinkLimit(AbstractLink source,PIDLimitEvent event) {}
+		}
+		)
+
+		lenLin2.setText(""+limb.getDH_R(0))
+		lenLin3.setText(""+follower.getDH_R(2))
+		lenLin4.setText(""+follower.getDH_R(1))
+		lenLin1.setText(""+(-follower.getRobotToFiducialTransform().getY()))
+		
+		velocityOfCrank.setText("360")
 		
 		update(null)
 	}
